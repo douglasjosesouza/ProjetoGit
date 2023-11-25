be6674ccf3966187d32eb05dc4dadebbdef7b287
dsouza <dsouza@dsouza-Not-Specified>
2023-11-25 11:31:42 -0300

    version 1.001.002

--- screenmatch/src/screenmatch/Principal.java
+++ screenmatch/src/screenmatch/Principal.java
@@ -1,11 +1,13 @@
 package screenmatch;
 
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
 	public static void main(String[] args) {
 		Filme meuFilme = new Filme();	
-		meuFilme.nome ="Alien o oitavo passageiro";
-		meuFilme.anoDeLancamento = 1970;
-		meuFilme.duracaoEmMinutos = 180;
+		meuFilme.setNome("Alien o oitavo passageiro");
+		meuFilme.setAnoDeLancamento(1970);
+		meuFilme.setDuracaoEmMinutos(180); 
 		
 		
 		meuFilme.exibeFichaTecnica();
@@ -14,8 +16,8 @@ public class Principal {
 		meuFilme.avalia(5);
 		meuFilme.avalia(10);
 		
-		System.out.println(meuFilme.somaDasAvaliacoes);
-		System.out.println(meuFilme.totalDeAvaliacoes);
+
+		System.out.println("Total de avaliações "+meuFilme.getTotalDeAvaliacoes());
 		System.out.println(meuFilme.pegaMedia());
 
 	}
